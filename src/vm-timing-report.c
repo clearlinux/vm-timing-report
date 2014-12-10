23e9cfb457013a5a2642f23d7deb23f4cf657dbe
Ikey Doherty <michael.i.doherty@intel.com>
2014-12-10 14:04:27 +0000

    Fix typo in comment header
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- src/vm-timing-report.c
+++ src/vm-timing-report.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2014 Intel Corporation
  * Author: Ikey Doherty <michael.i.doherty@intel.com>
  *
- * vm-timing-eport is free software; you can redistribute it and/or
+ * vm-timing-report is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
